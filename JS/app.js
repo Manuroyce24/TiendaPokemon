3776241ef4a6cd2b759a9e961d8f147849bde2e2
Manuroyce24 <95409974+Manuroyce24@users.noreply.github.com>
2022-10-19 23:31:07 +0200

    tienda

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+//Crear boton que cambie el color de fondo
+var boton = document.createElement("input");
+boton.setAttribute("onclick","cambiarFondo();")
+boton.setAttribute("value","Tema Oscuro");
+boton.setAttribute("type","submit");
+document.querySelector('div').appendChild(boton);
+function cambiarFondo(){
+    if(document.querySelector('div').style.background=="rgb(254, 255, 239)"){
+        document.querySelector('div').style.color="rgb(75, 247, 230)"; 
+        document.querySelector('div').style.background="rgb(9, 9, 67)";
+    }else{
+        document.querySelector('div').style.color="black";
+        document.querySelector('div').style.background="rgb(254, 255, 239)"; 
+    }
+}
