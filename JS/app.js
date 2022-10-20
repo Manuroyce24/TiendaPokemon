5b32a0e2c83ccca196a5901d244ed6602ea8007b
Manuel Recio Claros <manuroyce@outlook.com>
2022-10-20 12:27:05 +0200

    a

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,11 @@
-//Crear boton que cambie el color de fondo
-var boton = document.createElement("input");
-boton.setAttribute("onclick","cambiarFondo();")
-boton.setAttribute("value","Tema Oscuro/Claro");
-boton.setAttribute("type","submit");
-document.querySelector('div').appendChild(boton);
-function cambiarFondo(){
-    if(document.querySelector('div').style.background=="rgb(254, 255, 239)"){
-        document.querySelector('div').style.color="rgb(75, 247, 230)"; 
-        document.querySelector('div').style.background="rgb(9, 9, 67)";
-    }else{
-        document.querySelector('div').style.color="black";
-        document.querySelector('div').style.background="rgb(254, 255, 239)"; 
-    }
+//Boton que cambia a tema oscuro
+function darkMode(){
+    var element = document.body.getElementsByTagName('div')[0];
+    element.classList.toggle("dark-mode");
+}
+
+function enviar(pagina){
+    document.nombreDelFormulario.action = pagina;
+    document.nombreDelFormulario.submit();
+    
 }
