b6fccc3901788445efab9165e48d2b3b1d22e125
Manuroyce24 <95409974+Manuroyce24@users.noreply.github.com>
2022-10-20 15:02:46 +0200

    Terminado

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,11 @@
 //Boton que cambia a tema oscuro
-function darkMode(){
+function darkMode() {
     var element = document.body.getElementsByTagName('div')[0];
     element.classList.toggle("dark-mode");
 }
 
-function enviar(pagina){
+function enviar(pagina) {
     document.nombreDelFormulario.action = pagina;
     document.nombreDelFormulario.submit();
-    
+
 }
